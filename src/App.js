6e586fd4596f232efd4beaff146e8e50cc031cc0
DII UTAGS <miguelangel.sf@outlook.com>
2022-06-19 22:14:20 -0500

    feat: Creation of connection -First array get connection creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React, { useState } from 'react'
 import './App.css';
+import { useFetch } from './useFetch'
 
-function App() {
+
+
+const App = () => {
+  const [url, setUrl]=useState('https://pokeapi.co/api/v2/pokemon')
+  const state= useFetch(url)
+  const {charged,data}=state
+  charged? console.log('charged'):console.log(data.results)
+  return (
+    <div>
+
+    </div>
+  )
  
 }
 
