c60c9ec233c423c316b8f4f8dcdd26c88aedec0d
DII UTAGS <miguelangel.sf@outlook.com>
2022-06-19 21:57:10 -0500

    feat: New file created for connection with the api -component to make connections with the api

---
+++ src/useFetch.js
@@ -0,0 +1,23 @@
+import React , {useState,useEffect}from 'react'
+
+export const useFetch = (url) => {
+    const [result,setResult]=useState({charged:true,data:null})
+
+    useEffect( ()=>{
+        getDatos(url)
+    },{url})
+
+    async function getDatos(url){
+        try{
+        setResult({charged:true,data:null})
+        const resp=await fetch(url)
+        const data=await resp.json()
+        setResult({charged:false,data})
+        }
+        catch(e){
+            console.log(e)
+        }
+    }
+
+    return result
+}
