67c4e8a81ba31f1398200f05861d00baf4fca22d
DII UTAGS <miguelangel.sf@outlook.com>
2022-06-23 00:19:04 -0500

    Feat and Style: Navigation functionality and style to the main presentation -Route calls changed -Navigation functionality according to routes -Styling the card structure and data collection

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react'
-import './App.css';
-import { UseFetch } from './UseFetch'
-import { Cards } from './cards/Cards';
-
-
-
-const App = () => {
-  const [url, setUrl]=useState('https://pokeapi.co/api/v2/pokemon')
-  const state= UseFetch(url)
-  const {charged,data}=state
-  charged? console.log('charged'):console.log(data.results)
+import React from 'react';
+import PokeFun from './FunctionPoke';
+import './style.css'
+function App() {
   return (
-    <div>
-      {
-        charged
-        ?
-        <h1>Cargando...</h1>
-        :
-        <Cards results={data.results}/>
-      }
-
-    </div>
-  )
- 
+    <>
+      <PokeFun/>
+    </>
+  );
 }
 
 export default App;
