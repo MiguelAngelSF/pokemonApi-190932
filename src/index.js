ff840904cc51933b8383552f470d60436b398f21
DII UTAGS <miguelangel.sf@outlook.com>
2022-06-27 00:09:45 -0500

    Feat: React router implementation -Routing functionality for navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-ReactDOM.render(
-  <React.StrictMode>
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 
