08bf34612d55c6af4d99145ab6628aabae245d19
DII UTAGS <miguelangel.sf@outlook.com>
2022-07-15 23:51:36 -0500

    Style: Update variables for language translation

--- src/190932.jsx
+++ src/190932.jsx
@@ -3,8 +3,15 @@ import  Navbar  from './navbar';
 import perfil from './img/miguelperfil.jpeg'
 import UserContext from './context/userContext';
 import MoreInfo from './MoreInfo';
+import { useTranslation } from 'react-i18next';
+import { changeLanguage } from 'i18next';
 const About=()=>{
+    const { i18n, t } = useTranslation();
 
+    function changeLaguage(language) {
+      i18n.changeLanguage(language);
+    }
+  
 const userData = {
     grupo: "IDGS9A",
     matricula: 190932,
@@ -26,7 +33,7 @@ const userData = {
             </div>
             <div class="card-flap flap1">
                 <div class="card-description">
-                Universidad Tecnologica de Aguascalientes
+                {t("uni")}
                 </div>
                 <div class="card__image-holder">
                 <img class="card__image" src={perfil} width="200" height="200" alt="" />
